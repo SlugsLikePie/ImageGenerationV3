0f21333822c72566f1a7281caca13fd2453127ee
SlugsLikePie <slugslikepie@gmail.com>
2025-06-13 12:42:25 -0400

    Implemented rectangularAreaColorAverage and added a test image

--- src/App.java
+++ src/App.java
@@ -1,8 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        EditableImage img = new EditableImage();
+        EditableImage img = new EditableImage("Background.jpg");
         ImageEditor imgE = new ImageEditor(img);
         
-        // img = imgE.rectangularBlur();
+        img = imgE.rectangularBlur(1, 1, 1);
+
+        img.writeFile();
     }
 }
--- src/ImageEditor.java
+++ src/ImageEditor.java
@@ -1,59 +1,98 @@
 import java.awt.image.BufferedImage;
+import java.time.Duration;
+import java.time.Instant;
 import java.awt.Color;
 
 public class ImageEditor {
     EditableImage editingImg;
-    BufferedImage editingBufferedImg;
+    BufferedImage editingBImg;
+
+    int editingImgWidth;
+    int editingImgHeight;
+
     ImageEditorMode mode = ImageEditorMode.RETURN_AND_REPLACE;
 
     public ImageEditor() {
         editingImg = new EditableImage();
-        editingBufferedImg = editingImg.getBufferedImage();
+        editingBImg = editingImg.getBufferedImage();
+        editingImgWidth = editingBImg.getWidth();
+        editingImgHeight = editingBImg.getHeight();
     }
 
     public ImageEditor(ImageEditorMode mode) {
         editingImg = new EditableImage();
-        editingBufferedImg = editingImg.getBufferedImage();
+        editingBImg = editingImg.getBufferedImage();
+        editingImgWidth = editingBImg.getWidth();
+        editingImgHeight = editingBImg.getHeight();
         this.mode = mode;
     }
 
     public ImageEditor(EditableImage editingImage) {
         this.editingImg = editingImage;
-        editingBufferedImg = editingImage.getBufferedImage();
+        editingBImg = editingImage.getBufferedImage();
+        editingImgWidth = editingBImg.getWidth();
+        editingImgHeight = editingBImg.getHeight();
     }
 
     public ImageEditor(EditableImage editingImage, ImageEditorMode mode) {
         this.editingImg = editingImage;
-        editingBufferedImg = editingImage.getBufferedImage();
+        editingBImg = editingImage.getBufferedImage();
+        editingImgWidth = editingBImg.getWidth();
+        editingImgHeight = editingBImg.getHeight();
         this.mode = mode;
     }
 
     public EditableImage output(BufferedImage outputImg) {
         if (mode == ImageEditorMode.RETURN_AND_REPLACE) {
-            editingImg = new EditableImage();
+            editingBImg = outputImg;
         }
 
         return new EditableImage(outputImg);
     }
 
-    // private int rectangularAreaColorAverage(int x, int y, int xSize, int ySize) {
-        
-    // }
+    private int rectangularAreaColorAverage(int x, int y, int xRadius, int yRadius) {
+        int rSum = 0;
+        int gSum = 0;
+        int bSum = 0;
 
-    public EditableImage rectangularBlur(int iterations, int xSize, int ySize) {
+        int numPx = 0;
+
+        for (int xI = x - xRadius; xI <= x + xRadius; xI++)
+        for (int yI = y - yRadius; yI <= y + yRadius; yI++) {
+            if (xI >= 0 && xI < editingImgWidth && yI >= 0 && yI < editingImgHeight) {
+                int rgb = editingBImg.getRGB(xI, yI);
+                // System.out.println(rgb);
+
+                rSum += (rgb >> 16) & 0xFF;
+                gSum += (rgb >> 8) & 0xFF;
+                bSum += rgb & 0xFF;
+                
+                numPx++;
+            }
+        }
+
+        return ((rSum / numPx) << 16) + ((gSum / numPx) << 8) + (bSum / numPx);
+    }
+
+    public EditableImage rectangularBlur(int iterations, int xRadius, int yRadius) {
+        Instant start = Instant.now();
+        
         BufferedImage outputImg = new BufferedImage(
-            editingBufferedImg.getWidth(), 
-            editingBufferedImg.getHeight(), 
-            editingBufferedImg.getType()
+            editingBImg.getWidth(), 
+            editingBImg.getHeight(), 
+            editingBImg.getType()
         );
 
         for (int i = 0; i < iterations; i++)
-        for (int x = 0; x < editingBufferedImg.getWidth(); x++)
-        for (int y = 0; y < editingBufferedImg.getHeight(); y++) {
+        for (int x = 0; x < editingBImg.getWidth(); x++)
+        for (int y = 0; y < editingBImg.getHeight(); y++) {
             outputImg.setRGB(
-                x, y, 1324
+                x, y, rectangularAreaColorAverage(x, y, xRadius, yRadius)
             );
         }
+
+        Instant end = Instant.now();
+        System.out.println("Rectangular blurring completed in " + Duration.between(start, end).toMillis() + " milliseconds");
 
         return output(outputImg);
     }
